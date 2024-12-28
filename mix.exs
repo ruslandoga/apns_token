3ddaca757582f4ebe33811d32ce35d72ad3a635f
ruslandoga <ruslandoga+gh@icloud.com>
2024-12-28 05:56:34 +0000

    simpler ci

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule APNSToken.MixProject do
     [
       app: :apns_token,
       version: @version,
-      elixir: "~> 1.15",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       # hex
