e801e3fa950569515e11f26966205eb8ec79f70c
ruslandoga <ruslandoga+gh@icloud.com>
2025-12-07 16:41:50 +0300

    update deps

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule APNSToken.MixProject do
     [
       {:jose, "~> 1.11"},
       {:jason, "~> 1.4", optional: true},
-      {:ex_doc, "~> 0.36.1", only: :dev},
+      {:ex_doc, "~> 0.39.2", only: :dev},
       {:dialyxir, "~> 1.4", only: [:test, :dev], runtime: false}
     ]
   end
